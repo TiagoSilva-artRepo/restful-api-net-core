52e491ac90647ec81dc7af690017ab6a113c9d0d
Tiago Silva <tiago.silva@contisystems.pt>
2021-04-15 09:47:23 +0100

    Initialize projet

---
+++ RestfulAPI/Controllers/AuthorsController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using RestfulAPI.Helpers;
+using RestfulAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulAPI.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseLibraryRepository _courseLibraryRepository;
+        private readonly IMapper _mapper;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository, IMapper mapper)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet()]
+        public ActionResult<IEnumerable<AuthorDto>> GetAuthors()
+        {
+            var authorsFromRepo = _courseLibraryRepository.GetAuthors();
+            return Ok(_mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo));
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var author = _courseLibraryRepository.GetAuthor(authorId);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return Ok(_mapper.Map<AuthorDto>(author));
+        }
+
+    }
+}
---
+++ RestfulAPI/Profiles/AuthorsProfile.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using CourseLibrary.API.Entities;
+using RestfulAPI.Helpers;
+using RestfulAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulAPI.Profiles
+{
+    public class AuthorsProfile : Profile
+    {
+        public AuthorsProfile()
+        {
+            CreateMap<Author, AuthorDto>()
+                .ForMember(
+                    dest => dest.Name,
+                    opt => opt.MapFrom(src => $"{src.FirstName} {src.LastName}"))
+                .ForMember(
+                    dest => dest.Age,
+                    opt => opt.MapFrom(src => src.DateOfBirth.GetCurrentAge()));
+            ;
+        }
+    }
+}
