2708ce433432eafb752ff863f058f70e5337cd34
Tiago Silva <tiago.silva@contisystems.pt>
2021-05-27 15:04:39 +0100

    add validation module

--- RestfulAPI/Models/CourseForCreationDto.cs
+++ RestfulAPI/Models/CourseForCreationDto.cs
@@ -1,8 +1,26 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using RestfulAPI.ValidationAttributes;
+
 namespace RestfulAPI.Models
 {
-    public class CourseForCreationDto
+    public class CourseForCreationDto //: IValidatableObject
     {
+        [Required(ErrorMessage = "You should fill out a title")]
+        [MaxLength(100)]
         public string Title { get; set; }
+        [MaxLength(1500)]
+        [CourseTitleMustBeDifferentFromDescriptionAttribute]
         public string Description { get; set; }
+
+        /*public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Title == Description)
+            {
+                yield return new ValidationResult(
+                    "The provided description should be different from the title", new [] { "CourseForCreationDto" }
+                );
+            }
+        }*/
     }
 }
